023ac90635b97b32702753d19adffeea29ccb123
João Vitor Vitosoaski <joao.cz@hotmail.com>
2023-10-31 20:18:28 -0300

    changed the path of autoload.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require __DIR__.'/../vendor/autoload.php';
+require dirname(__FILE__, 2)."/vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
 $dotenv->load();
